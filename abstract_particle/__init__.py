1e19eae0344b33dafaa930372431b6e299af33a8
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2023-08-18 11:38:20 -0500

    REFACTOR particle class as independent

---
+++ abstract_particle/__init__.py
@@ -0,0 +1,4 @@
+version = '0.0.1'
+from .particle import Particle
+
+__all__ = ['Particle']
--- delphes_reader/particle/__init__.py
+++
@@ -1,4 +0,0 @@
-from Uniandes_Framework.delphes_reader.particle.electron_particle import ElectronParticle
-from Uniandes_Framework.delphes_reader.particle.jet_particle import JetParticle
-from Uniandes_Framework.delphes_reader.particle.met_particle import MetParticle
-from Uniandes_Framework.delphes_reader.particle.muon_particle import MuonParticle
