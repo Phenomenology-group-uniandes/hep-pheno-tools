5e172c13967c660fbdaea637e740a3b558703193
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2023-09-03 11:22:05 -0500

    ADD update function

--- __init__.py
+++ __init__.py
@@ -7,3 +7,17 @@ __all__ = [
     "delphes_reader",
     "lhe_reader",
 ]
+
+
+# get the lastest version from github when the framework is imported
+def update():
+    import os
+    import subprocess
+
+    print("Updating the framework from github...")
+    subprocess.run(
+        "git pull",
+        shell=True,
+        cwd=os.path.dirname(os.path.abspath(__file__)),
+    )
+    print("Done!")
