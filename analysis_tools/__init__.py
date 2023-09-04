6db1b88b5dc41dd3b4e1618a3819012e62de1531
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2023-09-03 20:56:05 -0500

    ADD tools to MG5

--- analysis_tools/__init__.py
+++ analysis_tools/__init__.py
@@ -1,7 +1,6 @@
 from .quiet import Quiet
 from . import histos_tools
 from .power_tests import approx_global_sig
-from .br_extractor import extract_branching_ratio_from_param_card
 from .dataframe_tools import generate_dataframe
 
 __all__ = [
