75228a1f4ce55c3b468a4b720afa4abc8c929918
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2023-09-04 10:59:28 +0000

    ADD __init__.py

--- madgraph_tools/__init__.py
+++ madgraph_tools/__init__.py
@@ -1,3 +1,11 @@
 from .runner import run_mg5
+from .seeds import get_new_seed
+from .seeds import get_seed_from_banner
+from .seeds import get_seeds_from_mg5_output_folder
 
-__all__ = ["run_mg5"]
+__all__ = [
+  "run_mg5",
+  "get_new_seed",
+  "get_seed_from_banner",
+  "get_seeds_from_mg5_output_folder"
+  ]
