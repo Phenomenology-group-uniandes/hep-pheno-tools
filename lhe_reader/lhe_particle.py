0d87384032bedbf85f0fb055adad7fcbbc074460
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2024-02-28 13:04:11 +0000

    FIX Charge issue LHE

--- lhe_reader/lhe_particle.py
+++ lhe_reader/lhe_particle.py
@@ -1,21 +1,22 @@
-from ..abstract_particle import Particle
 import numpy as np
+
+from ..abstract_particle import Particle
 
 
 class LHEParticle(Particle):
     def __init__(self, pdgid, spin, px=0, py=0, pz=0, energy=0, mass=0):
         self.pdgid = pdgid
         typep = abs(pdgid)
-        if ((typep == 2) or (typep == 4) or (typep == 6)):
-            charge = np.sign(pdgid)*2./3.
-        elif ((typep == 1) or (typep == 3) or (typep == 5)):
-            charge = -np.sign(pdgid)*1./3.
-        elif ((typep == 11) or (typep == 13) or (typep == 15)):
+        if (typep == 2) or (typep == 4) or (typep == 6):
+            charge = np.sign(pdgid) * 2.0 / 3.0
+        elif (typep == 1) or (typep == 3) or (typep == 5):
+            charge = -np.sign(pdgid) * 1.0 / 3.0
+        elif (typep == 11) or (typep == 13) or (typep == 15):
             charge = -np.sign(pdgid)
         else:
             charge = 0.0
 
-        super().__init__(charge=charge)
+        super().__init__(charge=float(charge))
         self.px = px
         self.py = py
         self.pz = pz
