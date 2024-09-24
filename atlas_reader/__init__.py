6aa6b4291d825a2ee8aa4c485834619243a6921b
cfrc2694 <c.rodriguez45@uniandes.edu.co>
2024-09-24 21:00:21 +0000

    ADD init to atlas reader

---
+++ atlas_reader/__init__.py
@@ -0,0 +1,4 @@
+from .classifier import get_good_jets, get_good_leptons
+from .helpers import download_atlas_opendataset
+
+__all__ = ["get_good_jets", "get_good_leptons", "download_atlas_opendataset"]
